97124c04ec464cc7bbcb8cab358fd09a36878cd2
nilraj9800 <1122nilraj@gmail.com>
2022-04-18 21:57:56 +0200

    second commit

--- crm/app/models.py
+++ crm/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+
+class User(AbstractUser):
+    pass
+
+class app(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+
+class Agent(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
