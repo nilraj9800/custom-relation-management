e30991337e3f450741592ade66dc0f1612c30bad
nilraj9800 <1122nilraj@gmail.com>
2022-05-02 21:13:10 +0200

    second commit

--- crm/app/admin.py
+++ crm/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import User,app,Agent
 # Register your models here.
+
+admin.site.register(User)
--- crm/app/models.py
+++ crm/app/models.py
@@ -12,11 +12,14 @@ class app(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.email
 
 class Agent(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     
-
+    def __str__(self):
+        return self.user.email
 
 
 
